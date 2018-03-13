6d8acca0fcf87f81ada618752e48b6b29fb87b81
Alex Storm <alex6614@edu.easj.dk>
2018-03-13 10:52:28 +0100

    Initial commit

---
+++ app/src/main/java/com/google/firebase/quickstart/auth/BirdWatching.kt
@@ -0,0 +1,12 @@
+package com.google.firebase.quickstart.auth
+
+import android.app.Activity
+import android.os.Bundle
+
+class BirdWatching : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bird_watching)
+    }
+}
